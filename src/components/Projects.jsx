a5bed517e933bd5c5fd1ad6d83d948d9bd080d5b
Ritik Bhardwaj <varribhar@gmail.com>
2022-05-31 12:09:12 +0530

    updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -22,7 +22,7 @@ const Projects = () => {
                 <p className='my-2 text-sm text-left mx-3 text-gray-500'>Bath & Body Works, LLC. is an American retail store chain that sells soaps, lotions, fragrances, and candles.</p>
                 <p className='my-2 text-sm text-left ml-3 mr-2  font-bold text-[#DC143C] inline'>Tech Stack:</p> <p className='my-2 text-sm text-left  text-gray-500 inline'>React | Tailwind | Rest API | NPM </p>
                 <div className=" flex flex-row px-3 py-8  justify-between w-full text-gray-500">
-                  <a href="https://bathandbodyworksclone.netlify.app/" target="_blank"><button className="flex flex-row justify-around w-[100px] text-white  text-lg hover:bg-[#fc5979]  rounded-xl bg-[#DC143C] ">live<FaEye className="mt-1"/></button></a>
+                  <a href="https://bathandbodyworksclone.netlify.app/" target="_blank"><button className="flex flex-row justify-around w-[100px] text-white  text-lg hover:bg-[#fc5979]  rounded-xl bg-[#DC143C] ">Live<FaEye className="mt-1"/></button></a>
                   <a href="https://github.com/ribhar/Bath-BodyWorks" target="_blank"><button className="flex flex-row justify-around w-[100px] text-white  text-lg hover:bg-[#fc5979]  rounded-xl bg-[#DC143C] ">Repo<FaGithub className="mt-1"/></button></a>
                 </div>
                 
@@ -33,7 +33,7 @@ const Projects = () => {
                 <p className='my-2 text-sm text-left mx-3 text-gray-500'>LinkedIn Learning provides video courses taught by industry experts in software, creative, and business skills.</p>
                 <p className='my-2 text-sm text-left ml-3 mr-2 font-bold text-[#DC143C] inline'>Tech Stack:</p> <p className='my-2 text-sm text-left  text-gray-500 inline'>HTML | CSS | JS | Modules </p>
                 <div className=" flex flex-row px-3 py-8 justify-between w-full text-gray-500">
-                  <a href="https://projectlynda.netlify.app/" target="_blank"><button className="flex flex-row justify-around w-[100px] text-white  text-lg hover:bg-[#fc5979]  rounded-xl bg-[#DC143C] ">live<FaEye className="mt-1"/></button></a>
+                  <a href="https://projectlynda.netlify.app/" target="_blank"><button className="flex flex-row justify-around w-[100px] text-white  text-lg hover:bg-[#fc5979]  rounded-xl bg-[#DC143C] ">Live<FaEye className="mt-1"/></button></a>
                   <a href="https://github.com/ribhar/LinkedinLearning" target="_blank"><button className="flex flex-row justify-around w-[100px] text-white  text-lg hover:bg-[#fc5979]  rounded-xl bg-[#DC143C] ">Repo<FaGithub className="mt-1"/></button></a>
                 </div>
                 
@@ -44,7 +44,7 @@ const Projects = () => {
                 <p className='my-2 text-sm text-left mx-3 text-gray-500'>PharmEasy is one of India's most trusted online pharmacy & medical stores offering pharmaceutical and healthcare products.</p>
                 <p className='my-2 text-sm text-left ml-3 mr-2 font-bold text-[#DC143C] inline'>Tech Stack:</p> <p className='my-2 text-sm text-left  text-gray-500 inline'> HTML | CSS | JS </p>
                 <div className=" flex flex-row px-3 py-8 justify-between w-full text-gray-500">
-                  <a href="https://pharmeasyin.netlify.app/" target="_blank"><button className="flex flex-row justify-around w-[100px] text-white  text-lg hover:bg-[#fc5979]  rounded-xl bg-[#DC143C] ">live<FaEye className="mt-1"/></button></a>
+                  <a href="https://pharmeasyin.netlify.app/" target="_blank"><button className="flex flex-row justify-around w-[100px] text-white  text-lg hover:bg-[#fc5979]  rounded-xl bg-[#DC143C] ">Live<FaEye className="mt-1"/></button></a>
                   <a href="https://github.com/ribhar/PharmEasy-clone" target="_blank"><button className="flex flex-row justify-around w-[100px] text-white  text-lg hover:bg-[#fc5979]  rounded-xl bg-[#DC143C] ">Repo<FaGithub className="mt-1"/></button></a>
                 </div>
                 
@@ -55,7 +55,7 @@ const Projects = () => {
                 <p className='my-2 text-sm text-left mx-3 text-gray-500'>YouTube is an American online video sharing and social media platform. A web application to watch online videos.</p>
                 <p className='my-2 text-sm text-left ml-3 mr-2 font-bold text-[#DC143C] inline'>Tech Stack:</p> <p className='my-2 text-sm text-left  text-gray-500 inline'> HTML | CSS | JS | Rest API </p>
                 <div className=" flex flex-row px-3 py-8 justify-between w-full text-gray-500">
-                  <a href="https://youtube-i.netlify.app/" target="_blank"><button className="flex flex-row justify-around w-[100px] text-white  text-lg hover:bg-[#fc5979]  rounded-xl bg-[#DC143C] ">live<FaEye className="mt-1"/></button></a>
+                  <a href="https://youtube-i.netlify.app/" target="_blank"><button className="flex flex-row justify-around w-[100px] text-white  text-lg hover:bg-[#fc5979]  rounded-xl bg-[#DC143C] ">Live<FaEye className="mt-1"/></button></a>
                   <a href="https://github.com/ribhar/Youtube" target="_blank"><button className="flex flex-row justify-around w-[100px] text-white  text-lg hover:bg-[#fc5979]  rounded-xl bg-[#DC143C] ">Repo<FaGithub className="mt-1"/></button></a>
                 </div>
                 
@@ -66,7 +66,7 @@ const Projects = () => {
                 <p className='my-2 text-sm text-left mx-3 text-gray-500'>Foody Mood is an online recipe book, helps you to become your own favorite cook. 'Foody Mood', your own recipe book.</p>
                 <p className='my-2 text-sm text-left ml-3 mr-2 font-bold text-[#DC143C] inline'>Tech Stack:</p> <p className='my-2 text-sm text-left  text-gray-500 inline'> HTML | CSS | JS | Rest API </p>
                 <div className=" flex flex-row px-3 py-8 justify-between w-full text-gray-500">
-                  <a href="https://foodymood.netlify.app/" target="_blank"><button className="flex flex-row justify-around w-[100px] text-white  text-lg hover:bg-[#fc5979]  rounded-xl bg-[#DC143C] ">live<FaEye className="mt-1"/></button></a>
+                  <a href="https://foodymood.netlify.app/" target="_blank"><button className="flex flex-row justify-around w-[100px] text-white  text-lg hover:bg-[#fc5979]  rounded-xl bg-[#DC143C] ">Live<FaEye className="mt-1"/></button></a>
                   <a href="https://github.com/ribhar/foodyMood" target="_blank"><button className="flex flex-row justify-around w-[100px] text-white  text-lg hover:bg-[#fc5979]  rounded-xl bg-[#DC143C] ">Repo<FaGithub className="mt-1"/></button></a>
                 </div>
                 
