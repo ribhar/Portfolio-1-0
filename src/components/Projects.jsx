f8bc0fc3bccb31309a0dc3ddc0daa0deed3ffe94
Ritik Bhardwaj <varribhar@gmail.com>
2022-06-02 15:31:59 +0530

    first

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
   
   return (
     <div name="projects" className="w-full h-[2400px] sm:h-[1500px] md:h-[1100px] bg-[#fff] text-gray-300 ">
-      <div className="mx-[20px] sm:max-w-[720px] md:max-w-[1000px] lg:max-w-[1250px]  p-4 flex flex-col justify-center w-4/5 h-full">
+      <div className="mx-[20px] sm:max-w-[720px] md:max-w-[1000px] lg:max-w-[1250px]  p-4 flex flex-col justify-center w-5/5 h-full">
         
           <p className= 'mb-20 text-5xl font-bold w-[185px] border-b-4 border-[#DC143C] text-gray-900'>
             Projects
