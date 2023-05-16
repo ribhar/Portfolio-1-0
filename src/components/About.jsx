9e814d4262d78e8741a4748b7139f5ce3898f524
Ritik Bhardwaj <98750701+ribhar@users.noreply.github.com>
2023-05-16 23:18:59 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,16 +20,7 @@ const About = () => {
                     <p>Greetings! I'm Ritik, a software engineer, and I'm delighted to extend a warm welcome to all of you visiting my portfolio.</p>
                 </div>
                 <div>
-                    <p className='text-gray-500'>
-I am a skilled software developer with 2.8 years of experience specializing in the MERN stack. I have a strong command over MySQL, PostgreSQL, and MongoDB for efficient data storage and retrieval. My proficiency extends to Node.js and Express for server-side development, as well as React and Redux for building interactive and responsive user interfaces.
-
-In addition to my expertise in the MERN stack, I am well-versed in TypeScript, JavaScript, Python, and C++, enabling me to develop robust and scalable applications across various platforms. I have a solid understanding of CI/CD practices and utilize Git and GitHub for efficient version control and collaborative development.
-
-As a continuous learner, I am currently pursuing an MCA in AI & ML to enhance my knowledge in artificial intelligence and machine learning. This academic pursuit enables me to bring a data-driven approach and innovative problem-solving techniques to my software development projects.
-
-With a strong foundation in the MERN stack, proficiency in multiple programming languages, and a passion for leveraging AI and ML technologies, I am confident in my ability to contribute to cutting-edge software solutions. I am driven by a desire to deliver high-quality code, adhere to best practices, and collaborate effectively with cross-functional teams.
-
-I am eager to bring my skills, experience, and enthusiasm to a dynamic organization that values innovation and fosters professional growth.</p>
+                    <p className='text-gray-500'>Experienced software developer with 3 years of expertise in the MERN stack, including MySQL, PostgreSQL, MongoDB, Node.js, Express, React, and Redux. Proficient in TypeScript, JavaScript, Python, and C++, with a strong understanding of CI/CD, Git, and GitHub. Currently pursuing an MCA in AI & ML to enhance skills in data-driven development. Passionate about delivering high-quality code, following best practices, and collaborating with cross-functional teams. Seeking opportunities to contribute to innovative software solutions and foster professional growth.</p>
                 </div>
             </div>
         </div>
